eca1be6b24ac1b04ac66f9387bc63d3e5ab698e3
Tien Nguyen <tiennguyen2507@gmail.com>
2025-08-01 00:12:14 +0700

    add run port

---
+++ nuxt.dev.config.js
@@ -0,0 +1,14 @@
+// Development configuration
+export default {
+  // Server configuration
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+  },
+
+  // Environment variables
+  env: {
+    NUXT_HOST: "0.0.0.0",
+    NUXT_PORT: 3000,
+  },
+};
